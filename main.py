0eae8fe41807219ea61a6247d09921aab591147b
vinicius-akio <viniciusakiogit@gmail.com>
2026-07-02 11:52:53 -0300

    arquivo "fazer" recomendação e próximos passos

---
+++ main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+from controlador_agente_menu import router as agente_menu_router
+
+app = FastAPI()
+
+app.include_router (agente_menu_router)
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        port = 80,
+        reload = True
+    )
